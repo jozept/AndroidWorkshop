e484c25d84706a0c3e305dc9eb11ce82a17fbb70
jozept <jozept@msn.com>
2018-09-02 15:30:23 +0700

    First Commit

---
+++ app/src/main/java/com/arit/demo/demo01/MainActivity.java
@@ -0,0 +1,47 @@
+package com.arit.demo.demo01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvMsg) TextView tvMsg;
+
+
+ /*  private tvmsg  ok uuhh     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+
+
+//        final TextView tvMsg = findViewById(R.id.tvMsg);
+//
+//        Button btnHello = (Button) findViewById(R.id.btnHello);
+//        btnHello.setText("Hello John");
+//
+//        btnHello.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                tvMsg.setText("Hi john this custom massage");
+//            }
+//        });
+    }
+  /*  public void onClickHello(View view){this.tvmsg.settexk}*/
+
+  @OnClick(R.id.btnHello)
+    public void doClickHell(){
+      this.tvMsg.setText (getString(R.string.Himsg));
+  }
+}
